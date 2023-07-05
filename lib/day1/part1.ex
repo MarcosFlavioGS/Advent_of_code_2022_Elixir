1b3e859e970797831e888334961f680e0371440f
Marcos Flavio <mfghost69@gmail.com>
2023-07-04 22:25:06 -0300

    Feat :Finalizing D1P1

--- lib/day1/part1.ex
+++ lib/day1/part1.ex
@@ -1,10 +1,26 @@
 defmodule AdventOfCode.Day1.Part1 do
   def fd_most_calories do
-    File.read!("inputs/day1.txt")
-    |> String.split("\n")
-    |> Enum.chunk_by(fn(x) -> String.length(x) == 0 end)
-    |> Enum.take_every(2)
-    |> IO.inspect()
-    {:ok, 42}
+    result =
+      File.read!("inputs/day1.txt")
+      |> String.split("\n")
+      |> Enum.chunk_by(fn x ->
+        String.length(x) == 0
+      end)
+      |> Enum.take_every(2)
+      |> Enum.map(fn list ->
+        Enum.map(list, fn x ->
+          Integer.parse(x) |> elem(0)
+        end)
+      end)
+      |> Enum.map(fn list ->
+        Enum.reduce(list, fn x, y ->
+          x + y
+        end)
+      end)
+      |> Enum.reduce(fn x, y ->
+        if x > y, do: x, else: y
+      end)
+
+    {:ok, result}
   end
 end
