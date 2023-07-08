b783a0f01b9be02451ee33e919dd30f87bc536f1
Marcos Flavio <mfghost69@gmail.com>
2023-07-08 18:18:36 -0300

    Feat :Day 2 part1 completed

--- lib/day2/part1.ex
+++ lib/day2/part1.ex
@@ -3,7 +3,32 @@ defmodule AdventOfCode.Day2.D2P1 do
     result =
     File.read!("inputs/day2.txt")
     |> String.split("\n")
-    |> IO.inspect()
-    {:ok, 42}
+    |> List.delete("")
+    |> get_results()
+    |> Enum.sum()
+    {:ok, result}
+  end
+
+  defp get_results([]), do: []
+  defp get_results [first|list] do
+    result =
+    String.split(first, " ")
+    |> get_play_result()
+    [result | get_results(list)]
+  end
+
+  defp get_play_result [oponent, player] do
+    case {oponent, player} do
+      {"A", "X"} -> 3 + 1
+      {"A", "Y"} -> 6 + 2
+      {"A", "Z"} -> 0 + 3
+      {"B", "X"} -> 0 + 1
+      {"B", "Y"} -> 3 + 2
+      {"B", "Z"} -> 6 + 3
+      {"C", "X"} -> 6 + 1
+      {"C", "Y"} -> 0 + 2
+      {"C", "Z"} -> 3 + 3
+      _ -> 0
+    end
   end
 end
